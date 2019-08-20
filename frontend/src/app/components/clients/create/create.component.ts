10dfb4d654ef1fd0d05c6da0130e80f84e928993
cfrias <cfrias1369@yahoo.com>
2019-08-20 16:23:03 -0400

    minor tweak to AddProspectToClient feature

--- frontend/src/app/components/clients/create/create.component.ts
+++ frontend/src/app/components/clients/create/create.component.ts
@@ -47,7 +47,7 @@ export class CreateComponent implements OnInit {
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.backLink = '/prospects/list';
+      this.backLink = '../../prospects/list';
       this.createForm.get('firstName').setValue(params.firstName);
       this.createForm.get('phoneNumber1').setValue(params.phoneNumber1);
       this.createForm.get('initialContactDate').setValue(params.initialContactDate);
