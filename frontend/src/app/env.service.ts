23451df09c92bac6242c0a869fc8fabbba97920f
cfrias <cfrias1369@yahoo.com>
2019-08-25 19:04:13 -0400

    Updated to hit the api on the new IIS website that I just set up

--- frontend/src/app/env.service.ts
+++ frontend/src/app/env.service.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
 
-  readonly API_URI = 'http://localhost:4001'; // This will connect to the nodeJS Web API
-  // readonly API_URI = 'https://localhost:44383/api'; // This will connect to the .Net Web API
+  // readonly API_URI = 'http://localhost:4001'; // This will connect to the nodeJS Web API
+  //readonly API_URI = 'https://localhost:44383/api'; // This will connect to the .Net Web API
+  readonly API_URI = 'http://localhost:8080/api'; // This will connect to the .Net Web API
 
   constructor() { }
 }
