972afbd78c3987823a79e69bbc5c19d622842f39
cfrias <cfrias1369@yahoo.com>
2019-08-28 00:33:22 -0400

    Adapted the api uri to connect to the most recent API that I have gotten to work (the .Net API)

--- frontend/src/app/env.service.ts
+++ frontend/src/app/env.service.ts
@@ -6,8 +6,11 @@ import { Injectable } from '@angular/core';
 export class EnvService {
 
   // readonly API_URI = 'http://localhost:4001'; // This will connect to the nodeJS Web API
-  //readonly API_URI = 'https://localhost:44383/api'; // This will connect to the .Net Web API
-  readonly API_URI = 'http://localhost:8080/api'; // This will connect to the .Net Web API
+  // readonly API_URI = 'https://localhost:44383/api'; // This will connect to the .Net Web API
+  // readonly API_URI = 'http://localhost:8080/api'; // This will connect to the .Net Web API
+  // readonly API_URI = 'https://localhost:44307/api'; // This will connect to the .Net Web API
+  // readonly API_URI = 'https://corewebapp20190827124747.azurewebsites.net/api'; // This will connect to the .Net Core Web API in Azure
+  readonly API_URI = 'https://webapi20190827012025.azurewebsites.net/api'; // This will connect to the .Net Web API in Azure
 
   constructor() { }
 }
