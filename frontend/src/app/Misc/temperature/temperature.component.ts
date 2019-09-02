1cb8c92768c092630a59eedd261a51c101f6b70c
cfrias <cfrias1369@yahoo.com>
2019-09-02 12:52:47 -0400

    Added simple one way binding with interpolation

--- frontend/src/app/Misc/temperature/temperature.component.ts
+++ frontend/src/app/Misc/temperature/temperature.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemperatureComponent implements OnInit {
 
+  title = 'Temperature Conversion';
+
   constructor() { }
 
   ngOnInit() {
