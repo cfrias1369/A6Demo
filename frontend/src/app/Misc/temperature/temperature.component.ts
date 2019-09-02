61a8d700aea26e3713a24601d885d7f08f6a3a54
cfrias <cfrias1369@yahoo.com>
2019-09-02 13:02:34 -0400

    Added advanced one way binding with property binding

--- frontend/src/app/Misc/temperature/temperature.component.ts
+++ frontend/src/app/Misc/temperature/temperature.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TemperatureComponent implements OnInit {
 
   title = 'Temperature Conversion';
+  titleGraphicUrl = '/assets/images/tempconversion.jpg';
 
   constructor() { }
 
