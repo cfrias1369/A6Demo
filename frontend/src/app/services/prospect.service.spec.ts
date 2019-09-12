0f2122657dcb61997a96e001f73e95d38a75d0fc
cfrias <cfrias1369@yahoo.com>
2019-09-11 21:58:53 -0400

    Repaired autogenerated spec.ts

--- frontend/src/app/services/prospect.service.spec.ts
+++ frontend/src/app/services/prospect.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProspectService } from './prospect.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 describe('ProspectService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ],
+  }));
 
   it('should be created', () => {
     const service: ProspectService = TestBed.get(ProspectService);
